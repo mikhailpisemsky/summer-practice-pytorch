1cb3aadf50e686ae03d0cb8900f729cc7ce28490
mikhailpisemsky <pisemskijmihail@gmail.com>
2025-07-08 15:56:22 +0500

    homework4

--- lesson4-homework/models/cnn_models.py
+++ lesson4-homework/models/cnn_models.py
@@ -190,3 +190,96 @@ class CNNWithKernelSize(nn.Module):
         x = self.dropout(x)
         x = self.fc(x)
         return x
+
+class CNN2ConvLayers(nn.Module):
+    def __init__(self, input_channels=3, num_classes=10):
+        super().__init__()
+        self.conv1 = nn.Conv2d(input_channels, 32, 3, 1, 1)
+        self.conv2 = nn.Conv2d(32, 64, 3, 1, 1)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.fc1 = nn.Linear(64 * 8 * 8, 128)
+        self.fc2 = nn.Linear(128, num_classes)
+        self.dropout = nn.Dropout(0.25)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(x.size(0), -1)
+        x = F.relu(self.fc1(x))
+        x = self.dropout(x)
+        x = self.fc2(x)
+        return x
+
+class CNN4ConvLayers(nn.Module):
+    def __init__(self, input_channels=3, num_classes=10):
+        super().__init__()
+        self.conv1 = nn.Conv2d(input_channels, 32, 3, 1, 1)
+        self.conv2 = nn.Conv2d(32, 64, 3, 1, 1)
+        self.conv3 = nn.Conv2d(64, 128, 3, 1, 1)
+        self.conv4 = nn.Conv2d(128, 256, 3, 1, 1)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.adaptive_pool = nn.AdaptiveAvgPool2d((4, 4))
+        self.fc1 = nn.Linear(256 * 4 * 4, 256)
+        self.fc2 = nn.Linear(256, num_classes)
+        self.dropout = nn.Dropout(0.25)
+    
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = F.relu(self.conv3(x))
+        x = F.relu(self.conv4(x))
+        x = self.adaptive_pool(x)
+        x = x.view(x.size(0), -1)
+        x = F.relu(self.fc1(x))
+        x = self.dropout(x)
+        x = self.fc2(x)
+        return x
+
+class CNN6ConvLayers(nn.Module):
+    def __init__(self, input_channels=3, num_classes=10):
+        super().__init__()
+        self.conv1 = nn.Conv2d(input_channels, 32, 3, 1, 1)
+        self.conv2 = nn.Conv2d(32, 32, 3, 1, 1)
+        self.conv3 = nn.Conv2d(32, 64, 3, 1, 1)
+        self.conv4 = nn.Conv2d(64, 64, 3, 1, 1)
+        self.conv5 = nn.Conv2d(64, 128, 3, 1, 1)
+        self.conv6 = nn.Conv2d(128, 128, 3, 1, 1)
+        self.pool = nn.MaxPool2d(2)
+        self.adaptive_pool = nn.AdaptiveAvgPool2d((4, 4))
+        self.fc1 = nn.Linear(128 * 4 * 4, 256)
+        self.fc2 = nn.Linear(256, num_classes)
+    
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = F.relu(self.conv2(x))
+        x = self.pool(F.relu(self.conv3(x)))
+        x = F.relu(self.conv4(x))
+        x = F.relu(self.conv5(x))
+        x = F.relu(self.conv6(x))
+        x = self.adaptive_pool(x)
+        x = x.view(x.size(0), -1)
+        x = F.relu(self.fc1(x))
+        return self.fc2(x)
+
+class CNNWithResidualLayers(nn.Module):
+    def __init__(self, input_channels=3, num_classes=10):
+        super().__init__()
+        self.conv1 = nn.Conv2d(input_channels, 32, 3, 1, 1)
+        self.bn1 = nn.BatchNorm2d(32)
+        
+        self.res1 = ResidualBlock(32, 32)
+        self.res2 = ResidualBlock(32, 64, 2)
+        self.res3 = ResidualBlock(64, 128, 2)
+        
+        self.adaptive_pool = nn.AdaptiveAvgPool2d((4, 4))
+        self.fc = nn.Linear(128 * 4 * 4, num_classes)
+    
+    def forward(self, x):
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = self.res1(x)
+        x = self.res2(x)
+        x = self.res3(x)
+        x = self.adaptive_pool(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+        return x
